60f92b15cf9d0c6bb3afbb93315d11debff87795
dbarros1979 <daniel.barros@genesis.global>
2022-11-01 12:41:35 +0000

    Docker bug with line endings

--- server/jvm/docker-scripts/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-systemctl start postgresql-14
-systemctl enable sshd.service
-systemctl start sshd.service
-su -c "startServer" - "alpha"
-echo "Logged as alpha, starting server"
-tail -f /dev/null
