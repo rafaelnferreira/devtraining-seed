c9a0efe69cc81a7a4ef7349e8252be090d6fa617
Rafael Ferreira <rafael.ferreira@genesis.global>
2023-11-22 12:02:10 +0000

    adding notify and auth preferences.

--- server/jvm/alpha-dictionary-cache/build.gradle.kts
+++ server/jvm/alpha-dictionary-cache/build.gradle.kts
@@ -2,6 +2,7 @@
 // Add your genesis config dependencies here
 dependencies {
     implementation("global.genesis:auth-config:${properties["authVersion"]}")
+    implementation("global.genesis:genesis-notify-config:${properties["notifyVersion"]}")
 }
 
 description = "alpha-dictionary-cache"
